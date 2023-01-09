d46f2413af9bfc0f173047143a5084e401fb9f50
Hugo Rojas <hugo.rojas@inturn.co>
2023-01-09 13:51:42 -0500

    Add middleware

--- internal/http/rest/routes.go
+++ internal/http/rest/routes.go
@@ -1,6 +1,9 @@
 package rest
 
 import (
+	"io"
+	"net/http"
+
 	"github.com/hugo.rojas/custom-api/internal/http/rest/handlers"
 	"github.com/hugo.rojas/custom-api/internal/iface"
 	"github.com/uptrace/bunrouter"
@@ -20,9 +23,61 @@ func InitRoutes(service iface.Service) *bunrouter.CompatRouter {
 	h := handlers.New(service)
 	// 	// r.Use(Recoverer(resp))  CHECK Recoverer and Check if Resp is needed
 
-	r.WithGroup("/api/", func(g *bunrouter.CompatGroup) {
+	api := r.NewGroup("/api", bunrouter.Use(errorHandler))
+	api.WithGroup("/", func(g *bunrouter.CompatGroup) {
 		g.GET("/campaigns/:id", h.GetCampaign)
 	})
 
 	return r
+}
+
+func errorHandler(next bunrouter.HandlerFunc) bunrouter.HandlerFunc {
+
+	return func(w http.ResponseWriter, req bunrouter.Request) error {
+		err := next(w, req)
+
+		switch err := err.(type) {
+		case nil:
+			// no error
+		case HTTPError:
+			w.WriteHeader(err.statusCode)
+			_ = bunrouter.JSON(w, err)
+		default:
+			httpErr := NewHTTPError(err)
+			w.WriteHeader(httpErr.statusCode)
+			_ = bunrouter.JSON(w, httpErr)
+		}
+
+		return err
+	}
+}
+
+type HTTPError struct {
+	statusCode int
+
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+func (e HTTPError) Error() string {
+	return e.Message
+}
+
+func NewHTTPError(err error) HTTPError {
+	switch err {
+	case io.EOF:
+		return HTTPError{
+			statusCode: http.StatusBadRequest,
+
+			Code:    "eof",
+			Message: "EOF reading HTTP request body",
+		}
+	}
+
+	return HTTPError{
+		statusCode: http.StatusInternalServerError,
+
+		Code:    "internal",
+		Message: "Internal server error",
+	}
 }
